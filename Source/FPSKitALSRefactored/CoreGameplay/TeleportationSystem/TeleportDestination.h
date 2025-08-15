97f96723ecd1bb0e169b40e23dab9b1decfff397
Alex <balogi83@gmail.com>
2025-08-15 17:14:04 +0300

    Hiding unnecessary destination categories

--- Source/FPSKitALSRefactored/CoreGameplay/TeleportationSystem/TeleportDestination.h
+++ Source/FPSKitALSRefactored/CoreGameplay/TeleportationSystem/TeleportDestination.h
@@ -11,7 +11,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnActorFinishCooldown, ATeleportDes
 
 class UTeleportingSubsystem;
 
-UCLASS()
+UCLASS(HideCategories = ("Replication", "Rendering", "Collision", "HLOD", "Input", "Physics", "Networking", "Actor", "LevelInstance", "Cooking", "DataLayers"))
 class FPSKITALSREFACTORED_API ATeleportDestination : public AActor
 {
     GENERATED_BODY()
