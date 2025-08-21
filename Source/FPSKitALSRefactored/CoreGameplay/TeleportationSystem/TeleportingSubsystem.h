ec2d8633657852f933da2416725375c6179d4797
Alex <balogi83@gmail.com>
2025-08-21 16:25:04 +0300

    Delete commemnt

--- Source/FPSKitALSRefactored/CoreGameplay/TeleportationSystem/TeleportingSubsystem.h
+++ Source/FPSKitALSRefactored/CoreGameplay/TeleportationSystem/TeleportingSubsystem.h
@@ -65,7 +65,6 @@ public:
 	FOnTeleportation OnTeleportation;
 
     UPROPERTY(BlueprintAssignable, Category = "Teleportation")
-    //FOnTeleportationFailed OnTeleportationFailed;
     FOnTeleportationFailed OnTeleportationFailed;
 
     UPROPERTY(BlueprintAssignable, Category = "Teleportation")
