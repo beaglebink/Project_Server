b025c48edc9408658cc9fc37a5da93efd02e6cff
Oleg <oleh.ya@gmail.com>
2025-08-22 12:35:49 +0300

    SomeCorrectionsToUpdateMaterial

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Private/DiscreteSystem/A_DiscreteSystemNode.cpp
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Private/DiscreteSystem/A_DiscreteSystemNode.cpp
@@ -16,6 +16,8 @@ AA_DiscreteSystemNode::AA_DiscreteSystemNode()
 	SM_ZoneBorder->SetupAttachment(RootComponent);
 	SM_Node->SetupAttachment(RootComponent);
 	SKM_Node->SetupAttachment(RootComponent);
+
+	CurrentNodeNumber = NodeNumber;
 }
 
 void AA_DiscreteSystemNode::BeginPlay()
@@ -34,15 +36,16 @@ void AA_DiscreteSystemNode::UpdateBorderMaterial()
 {
 	CurrentNodeNumber = NodeNumber;
 
-	if (SM_ZoneBorder && SM_ZoneBorder->GetMaterial(0))
+	if (!DMI_BorderMaterial)
 	{
-		DMI_BorderMaterial = SM_ZoneBorder->CreateDynamicMaterialInstance(0, SM_ZoneBorder->GetMaterial(0));
-		if (DMI_BorderMaterial)
+		if (SM_ZoneBorder && SM_ZoneBorder->GetMaterial(0))
 		{
-			SM_ZoneBorder->SetMaterial(0, DMI_BorderMaterial);
-			DMI_BorderMaterial->SetScalarParameterValue(FName("NodeNumber"), CurrentNodeNumber);
+			DMI_BorderMaterial = SM_ZoneBorder->CreateDynamicMaterialInstance(0, SM_ZoneBorder->GetMaterial(0));
 		}
 	}
+
+	SM_ZoneBorder->SetMaterial(0, DMI_BorderMaterial);
+	DMI_BorderMaterial->SetScalarParameterValue(FName("NodeNumber"), CurrentNodeNumber);
 }
 
 void AA_DiscreteSystemNode::NodeSound()
