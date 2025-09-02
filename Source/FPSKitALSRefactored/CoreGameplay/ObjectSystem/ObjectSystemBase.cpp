cd9c3e51f13a6ee56376e235dfe11db8d7eefc5a
Alex <balogi83@gmail.com>
2025-09-02 18:17:07 +0300

    Test commands

---
+++ Source/FPSKitALSRefactored/CoreGameplay/ObjectSystem/ObjectSystemBase.cpp
@@ -0,0 +1,58 @@
+#include "ObjectSystemBase.h"
+#include <Kismet/KismetStringLibrary.h>
+
+TArray<FString> AObjectSystemBase::ParseCommands(const FText& InputText) const
+{
+	TArray<FString> Commands;
+
+	Commands = UKismetStringLibrary::ParseIntoArray(InputText.ToString(), TEXT("\n"), true);
+
+	return Commands;
+}
+
+void AObjectSystemBase::ParseText(const FText Text)  
+{  
+TArray<FString> Commands = ParseCommands(Text);  
+	for (auto Command : Commands)  
+	{  
+		FString TrimmedCommand = Command.TrimStartAndEnd();  
+		FString Left, Right;  
+		if (TrimmedCommand.Split(TEXT("="), &Left, &Right))  
+		{  
+			FString VariableName = *Left.TrimStartAndEnd();  
+			FString Value = *Right.TrimStartAndEnd();  
+			if (!VariableName.IsEmpty() && !Value.IsEmpty())  
+			{  
+				VariableAssignment(VariableName, Value);  
+			}  
+		}  
+		else  
+		{  
+			FString CommandName = *TrimmedCommand;  
+			if (!CommandName.IsEmpty())  
+			{  
+				TArray<FString> Arguments;  
+				int32 OpenParenIndex, CloseParenIndex;  
+				FString ParsedCommand = TEXT("");
+				if (TrimmedCommand.FindChar(TEXT('('), OpenParenIndex) && TrimmedCommand.FindChar(TEXT(')'), CloseParenIndex))  
+				{  
+					FString ArgumentsString = TrimmedCommand.Mid(OpenParenIndex + 1, CloseParenIndex - OpenParenIndex - 1);  
+					Arguments = UKismetStringLibrary::ParseIntoArray(ArgumentsString, TEXT(","), true);  
+					ParsedCommand = TrimmedCommand.Left(OpenParenIndex).TrimStartAndEnd();
+				}  
+				ExecuteCommand(ParsedCommand, Arguments);
+			}  
+		}  
+	}  
+}
+
+void AObjectSystemBase::VariableAssignment_Implementation(const FString& VariableName, const FString& Value)
+{
+
+}
+
+void AObjectSystemBase::ExecuteCommand_Implementation(const FString& Command, const TArray<FString>& Arguments)
+{
+
+}
+
---
+++ Source/FPSKitALSRefactored/CoreGameplay/ObjectSystem/ObjectSystemBase.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ObjectSystemBase.generated.h"
+
+UCLASS()
+class FPSKITALSREFACTORED_API AObjectSystemBase : public AActor
+{
+    GENERATED_BODY()
+
+public:
+	TArray<FString> ParseCommands(const FText& InputText) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ObjectSystem")
+	void ParseText(const FText Text);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ObjectSystem")
+	void VariableAssignment(const FString& VariableName, const FString& Value);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ObjectSystem")
+	void ExecuteCommand(const FString& Command, const TArray<FString>& Arguments);
+};
