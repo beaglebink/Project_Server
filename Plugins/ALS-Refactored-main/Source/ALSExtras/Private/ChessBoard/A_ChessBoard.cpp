d9348a0c46a2298d8c32f8a725929c68e2b64ce7
Oleg <oleh.ya@gmail.com>
2025-09-22 20:39:02 +0300

    CellsRotation

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Private/ChessBoard/A_ChessBoard.cpp
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Private/ChessBoard/A_ChessBoard.cpp
@@ -1,6 +1,7 @@
 #include "ChessBoard/A_ChessBoard.h"
 #include "PhysicsEngine/PhysicsConstraintComponent.h"
 #include "Components/BoxComponent.h"
+#include "Components/AudioComponent.h"
 #include "AlsCharacterExample.h"
 #include "Kismet/GameplayStatics.h"
 
@@ -9,7 +10,26 @@ AA_ChessBoard::AA_ChessBoard()
 	PrimaryActorTick.bCanEverTick = true;
 
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	RotateTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("RotateTimeline"));
 }
+
+#if WITH_EDITOR
+void AA_ChessBoard::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.Property)
+	{
+		FName PropertyName = PropertyChangedEvent.Property->GetFName();
+
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(AA_ChessBoard, MinTime) ||
+			PropertyName == GET_MEMBER_NAME_CHECKED(AA_ChessBoard, MaxTime))
+		{
+			MaxTime = FMath::Max(MaxTime, MinTime);
+		}
+	}
+}
+#endif
 
 void AA_ChessBoard::OnConstruction(const FTransform& Transform)
 {
@@ -21,6 +41,20 @@ void AA_ChessBoard::OnConstruction(const FTransform& Transform)
 void AA_ChessBoard::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (RotateFloatCurve)
+	{
+		RotateProgressFunction.BindUFunction(this, FName("RotateTimelineProgress"));
+		RotateTimeline->AddInterpFloat(RotateFloatCurve, RotateProgressFunction);
+
+		RotateFinishedFunction.BindUFunction(this, FName("RotateTimelineFinished"));
+		RotateTimeline->SetTimelineFinishedFunc(RotateFinishedFunction);
+
+		RotateTimeline->SetLooping(false);
+	}
+
+	FTimerHandle FirstTimerHandle;
+	GetWorldTimerManager().SetTimer(FirstTimerHandle, this, &AA_ChessBoard::ScheduleNextTimer, FMath::FRandRange(MinTime, MaxTime), false);
 }
 
 void AA_ChessBoard::Tick(float DeltaTime)
@@ -39,6 +73,21 @@ void AA_ChessBoard::BuildField()
 			Cell.MeshComponent->DestroyComponent();
 			Cell.MeshComponent = nullptr;
 		}
+		if (Cell.ConstraintComponent)
+		{
+			Cell.ConstraintComponent->DestroyComponent();
+			Cell.ConstraintComponent = nullptr;
+		}
+		if (Cell.TriggerComponent)
+		{
+			Cell.TriggerComponent->DestroyComponent();
+			Cell.TriggerComponent = nullptr;
+		}
+		if (Cell.AudioComponent)
+		{
+			Cell.AudioComponent->DestroyComponent();
+			Cell.AudioComponent = nullptr;
+		}
 	}
 	ChessField.Empty();
 
@@ -55,7 +104,6 @@ void AA_ChessBoard::BuildField()
 		for (int32 Col = 0; Col < Columns; ++Col)
 		{
 			FCell NewCell;
-			NewCell.bIsBlack = false;
 
 			// Mesh
 			FString CompName = FString::Printf(TEXT("Cell_%d_%d"), Row, Col);
@@ -65,13 +113,13 @@ void AA_ChessBoard::BuildField()
 			CellComp->RegisterComponent();
 			CellComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 			CellComp->SetRelativeLocation(FVector(Row * CellSize.X, Col * CellSize.Y, 0.0f));
-			if ((Row * Columns + Col) & 1)
+			CellComp->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+			if ((Row + Col) & 1)
 			{
-				NewCell.bIsBlack = true;
 				CellComp->SetRelativeRotation(FRotator(0.0f, 0.0f, 180.0f));
 			}
 			CellComp->SetSimulatePhysics(true);
-			CellComp->SetMassOverrideInKg(NAME_None, 10.f);
+			CellComp->SetMassOverrideInKg(NAME_None, 10.0f);
 			CellComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 			CellComp->SetCollisionObjectType(ECollisionChannel::ECC_PhysicsBody);
 
@@ -96,7 +144,8 @@ void AA_ChessBoard::BuildField()
 			UBoxComponent* Trigger = NewObject<UBoxComponent>(this, *TriggerName);
 			Trigger->RegisterComponent();
 			Trigger->AttachToComponent(CellComp, FAttachmentTransformRules::KeepRelativeTransform);
-			Trigger->SetBoxExtent(FVector(MeshExtent.X * 0.5f, MeshExtent.Y * 0.5f, MeshExtent.Z * 0.5f + 5));
+			Trigger->SetBoxExtent(FVector(MeshExtent.X * 0.45f, MeshExtent.Y * 0.45f, 5.0f));
+			Trigger->SetRelativeLocation(FVector(0.0f, 0.0f, -MeshExtent.Z * 0.5f));
 			Trigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 			Trigger->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
 			Trigger->SetCollisionResponseToAllChannels(ECR_Ignore);
@@ -104,8 +153,19 @@ void AA_ChessBoard::BuildField()
 			Trigger->OnComponentBeginOverlap.AddDynamic(this, &AA_ChessBoard::OnCellBeginOverlap);
 			Trigger->OnComponentEndOverlap.AddDynamic(this, &AA_ChessBoard::OnCellEndOverlap);
 
+			// Audio
+			FString AudioName = FString::Printf(TEXT("Audio_%d_%d"), Row, Col);
+			UAudioComponent* AudioComp = NewObject<UAudioComponent>(this, *AudioName);
+			AudioComp->RegisterComponent();
+			AudioComp->AttachToComponent(CellComp, FAttachmentTransformRules::KeepRelativeTransform);
+			AudioComp->bAutoActivate = false;
+			AudioComp->SetSound(RotateSound);
+
+			// Cell
 			NewCell.MeshComponent = CellComp;
-			NewCell.Trigger = Trigger;
+			NewCell.ConstraintComponent = Constraint;
+			NewCell.TriggerComponent = Trigger;
+			NewCell.AudioComponent = AudioComp;
 			ChessField.Add(NewCell);
 		}
 	}
@@ -117,10 +177,10 @@ void AA_ChessBoard::OnCellBeginOverlap(UPrimitiveComponent* OverlappedComp, AAct
 	{
 		FCell* HitCell = ChessField.FindByPredicate([OverlappedComp](const FCell& Cell)
 			{
-				return Cell.Trigger == OverlappedComp;
+				return Cell.TriggerComponent == OverlappedComp;
 			});
 
-		if (HitCell && HitCell->bIsBlack)
+		if (HitCell)
 		{
 			CharactersToCells.FindOrAdd(Ch).Add(HitCell);
 		}
@@ -133,10 +193,10 @@ void AA_ChessBoard::OnCellEndOverlap(UPrimitiveComponent* OverlappedComp, AActor
 	{
 		FCell* HitCell = ChessField.FindByPredicate([OverlappedComp](const FCell& Cell)
 			{
-				return Cell.Trigger == OverlappedComp;
+				return Cell.TriggerComponent == OverlappedComp;
 			});
 
-		if (HitCell && HitCell->bIsBlack)
+		if (HitCell)
 		{
 			if (TSet<FCell*>* Cells = CharactersToCells.Find(Ch))
 			{
@@ -156,5 +216,141 @@ void AA_ChessBoard::DoCharacterDamage()
 	{
 		UGameplayStatics::ApplyDamage(Pair.Key, CellDamage, Pair.Key->GetController(), this, nullptr);
 	}
+}
+
+void AA_ChessBoard::ScheduleNextTimer()
+{
+	if (2 * RotateTimeline->GetTimelineLength() > MinTime)
+	{
+		return;
+	}
+
+	RotateCells();
+
+	float Delay = FMath::FRandRange(MinTime, MaxTime);
+
+	FTimerHandle RandomTimerHandle;
+	GetWorldTimerManager().SetTimer(RandomTimerHandle, this, &AA_ChessBoard::ScheduleNextTimer, Delay, false);
+}
+
+void AA_ChessBoard::RotateCells()
+{
+
+	for (size_t Index = 0; Index < ChessField.Num(); ++Index)
+	{
+		if (abs(ChessField[Index].MeshComponent->GetComponentRotation().Pitch) > 90.0f || abs(ChessField[Index].MeshComponent->GetComponentRotation().Roll) > 90.0f)
+		{
+			TargetRotation = CountTargetRotationInDependsOnPrevRotation(ChessField[Index].MeshComponent->GetComponentRotation());
+
+			CellsToRotate.Add(Index);
+			ChessField[Index].AudioComponent->Play();
+			InitialRotations.Add(Index, ChessField[Index].MeshComponent->GetComponentRotation());
+		}
+	}
+	RotateTimeline->PlayFromStart();
+
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(TimerHandle, [this]()
+		{
+			int32 NumToRotate = static_cast<int32>(FMath::FRandRange(ChessField.Num() * 0.3f, ChessField.Num() * 0.4f));
+
+			for (size_t i = 0; i < ChessField.Num(); ++i)
+			{
+				CellsToRotate.Add(i);
+			}
+
+			for (int32 i = CellsToRotate.Num() - 1; i > 0; --i)
+			{
+				int32 j = FMath::RandRange(0, i);
+				CellsToRotate.Swap(i, j);
+			}
+			CellsToRotate.SetNum(NumToRotate);
+
+			for (int32 Index : CellsToRotate)
+			{
+				TargetRotation = CountTargetRotationInDependsOnPrevRotation(ChessField[Index].MeshComponent->GetComponentRotation());
+
+				ChessField[Index].AudioComponent->Play();
+				InitialRotations.Add(Index, ChessField[Index].MeshComponent->GetComponentRotation());
+			}
+
+			RotateTimeline->PlayFromStart();
+		}, RotateTimeline->GetTimelineLength(), false);
+}
+
+FQuat AA_ChessBoard::CountTargetRotationInDependsOnPrevRotation(const FRotator& CurrentRot)
+{
+	FRotator TargetRot = CurrentRot;
+
+	bool bIsWhiteField = (FMath::Abs(TargetRot.Pitch) < 90.0f && FMath::Abs(TargetRot.Roll) < 90.0f) || (FMath::Abs(TargetRot.Pitch) > 90.0f && FMath::Abs(TargetRot.Roll) > 90.0f);
+
+	bool bUsePitchAsLead = false;
+
+	if (bIsWhiteField)
+	{
+		if ((FMath::Abs(TargetRot.Pitch) < 90.0f && FMath::Abs(TargetRot.Roll) < 90.0f))
+		{
+			bUsePitchAsLead = FMath::Abs(TargetRot.Pitch) >= FMath::Abs(TargetRot.Roll);
+		}
+		else
+		{
+			bUsePitchAsLead = FMath::Abs(TargetRot.Pitch) <= FMath::Abs(TargetRot.Roll);
+		}
+	}
+	else
+	{
+		float DeltaPitch = FMath::Abs((FMath::Abs(TargetRot.Pitch) > 90.0f ? 180.0f : 0.0f) - FMath::Abs(TargetRot.Pitch));
+		float DeltaRoll = FMath::Abs((FMath::Abs(TargetRot.Roll) > 90.0f ? 180.0f : 0.0f) - FMath::Abs(TargetRot.Roll));
+		bUsePitchAsLead = DeltaPitch >= DeltaRoll;
+	}
+
+	if (bUsePitchAsLead)
+	{
+		float DeltaPitch = (FMath::Abs(TargetRot.Pitch) > 90.0f ? 0.0f : 180.0f) - TargetRot.Pitch;
+		TargetRot.Pitch += DeltaPitch;
+
+		float DeltaRoll = (FMath::Abs(TargetRot.Roll) > 90.0f ? 180.0f : 0.0f) - TargetRot.Roll;
+		TargetRot.Roll += DeltaRoll;
+	}
+	else
+	{
+		float DeltaPitch = (FMath::Abs(TargetRot.Pitch) > 90.0f ? 180.0f : 0.0f) - TargetRot.Pitch;
+		TargetRot.Pitch += DeltaPitch;
+
+		float DeltaRoll = (FMath::Abs(TargetRot.Roll) > 90.0f ? 0.0f : 180.0f) - TargetRot.Roll;
+		TargetRot.Roll += DeltaRoll;
+	}
+
+	TargetRot.Normalize();
+	return TargetRot.Quaternion();
+}
+
+void AA_ChessBoard::RotateTimelineProgress(float Value)
+{
+	for (int32 CellIndex : CellsToRotate)
+	{
+		if (ChessField.IsValidIndex(CellIndex))
+		{
+			ChessField[CellIndex].MeshComponent->SetSimulatePhysics(false);
+			FQuat NewOrientation = FQuat::Slerp(InitialRotations[CellIndex].Quaternion(), TargetRotation, Value);
+			ChessField[CellIndex].MeshComponent->SetWorldRotation(NewOrientation.Rotator());
+		}
+	}
+}
+
+void AA_ChessBoard::RotateTimelineFinished()
+{
+	for (int32 CellIndex : CellsToRotate)
+	{
+		if (ChessField.IsValidIndex(CellIndex))
+		{
+			ChessField[CellIndex].MeshComponent->SetSimulatePhysics(true);
+			ChessField[CellIndex].ConstraintComponent->SetConstrainedComponents(ChessField[CellIndex].MeshComponent, NAME_None, Cast<UPrimitiveComponent>(RootComponent), NAME_None);
+			ChessField[CellIndex].AudioComponent->Stop();
+		}
+	}
+
+	CellsToRotate.Empty();
+	InitialRotations.Empty();
 }
 
--- Plugins/ALS-Refactored-main/Source/ALSExtras/Public/ChessBoard/A_ChessBoard.h
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Public/ChessBoard/A_ChessBoard.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "Components/TimelineComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "A_ChessBoard.generated.h"
 
 class UBoxComponent;
 class AAlsCharacterExample;
+class UPhysicsConstraintComponent;
 
 USTRUCT(BlueprintType)
 struct FCell
@@ -17,10 +19,13 @@ public:
 	UStaticMeshComponent* MeshComponent;
 
 	UPROPERTY()
-	UBoxComponent* Trigger;
+	UPhysicsConstraintComponent* ConstraintComponent;
 
 	UPROPERTY()
-	uint8 bIsBlack : 1{false};
+	UBoxComponent* TriggerComponent;
+
+	UPROPERTY()
+	UAudioComponent* AudioComponent;
 };
 
 UCLASS()
@@ -30,6 +35,9 @@ class ALSEXTRAS_API AA_ChessBoard : public AActor
 
 public:
 	AA_ChessBoard();
+
+private:
+	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent);
 
 protected:
 	virtual void OnConstruction(const FTransform& Transform) override;
@@ -49,13 +57,29 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Field", meta = (AllowPrivateAccess = "true", ClampMin = "0", ClampMax = "50"))
 	float CellDamage;
 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Field|CellRotation", meta = (AllowPrivateAccess = "true", ClampMin = "0", ClampMax = "60"))
+	float MinTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Field|CellRotation", meta = (AllowPrivateAccess = "true", ClampMin = "0", ClampMax = "60"))
+	float MaxTime;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Field", meta = (AllowPrivateAccess = "true"))
 	UStaticMesh* CellMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Field", meta = (AllowPrivateAccess = "true"))
+	USoundBase* RotateSound;
 
 	UPROPERTY()
 	TArray<FCell> ChessField;
 
 	TMap<AAlsCharacterExample*, TSet<FCell*>> CharactersToCells;
+
+	TArray<int32> CellsToRotate;
+
+	TMap<int32, FRotator> InitialRotations;
+
+	FQuat TargetRotation;
 
 	void BuildField();
 
@@ -66,4 +90,27 @@ private:
 	void OnCellEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 	void DoCharacterDamage();
+
+	void ScheduleNextTimer();
+
+	void RotateCells();
+
+	FQuat CountTargetRotationInDependsOnPrevRotation(const FRotator& CurrentRot);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UTimelineComponent* RotateTimeline;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Timeline")
+	UCurveFloat* RotateFloatCurve;
+
+	FOnTimelineFloat RotateProgressFunction;
+
+	FOnTimelineEvent RotateFinishedFunction;
+
+	UFUNCTION()
+	void RotateTimelineProgress(float Value);
+
+	UFUNCTION()
+	void RotateTimelineFinished();
 };
