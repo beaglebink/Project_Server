3a272e09718553f1324a59ec127a65e3d78afd24
Oleg <oleh.ya@gmail.com>
2025-10-02 22:38:11 +0300

    Barrier

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Private/DeadlockSystem/A_Barrier.cpp
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Private/DeadlockSystem/A_Barrier.cpp
@@ -1,27 +1,94 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "DeadlockSystem/A_Barrier.h"
+#include "Components/BoxComponent.h"
+#include "Components/AudioComponent.h"
 
-// Sets default values
 AA_Barrier::AA_Barrier()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+
+	MeshComponent->SetupAttachment(RootComponent);
+	BoxComponent->SetupAttachment(MeshComponent);
+	AudioComponent->SetupAttachment(RootComponent);
+
+	MeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	MeshComponent->SetGenerateOverlapEvents(false);
+	MeshComponent->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+
+	BoxComponent->SetBoxExtent(FVector(100.f, 50.f, 50.f));
+	BoxComponent->SetGenerateOverlapEvents(true);
+	BoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	BoxComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
 }
 
-// Called when the game starts or when spawned
+void AA_Barrier::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!MaterialInstanceDynamic)
+	{
+		MaterialInstanceDynamic = MeshComponent->CreateAndSetMaterialInstanceDynamic(0);
+	}
+	if (MaterialInstanceDynamic)
+	{
+		MaterialInstanceDynamic->SetScalarParameterValue("IsOpen", 0.0f);
+	}
+}
+
 void AA_Barrier::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AA_Barrier::OnBarrierBeginOverlap);
+	BoxComponent->OnComponentEndOverlap.AddDynamic(this, &AA_Barrier::OnBarrierEndOverlap);
 }
 
-// Called every frame
 void AA_Barrier::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (bIsOverlapping)
+	{
+		MaterialInstanceDynamic->SetVectorParameterValue("ActorLocation", OverlappingActor->GetActorLocation());
+	}
+}
+
+void AA_Barrier::OnBarrierBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	bIsOverlapping = true;
+	OverlappingActor = OtherActor;
+}
+
+void AA_Barrier::OnBarrierEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	bIsOverlapping = false;
+}
+
+void AA_Barrier::GetTextCommand(FText Command)
+{
+	if (Command.EqualTo(BarrierPassword))
+	{
+		MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		MaterialInstanceDynamic->SetScalarParameterValue("IsOpen", 1.0f);
+
+		if (AudioComponent && AudioComponent->IsPlaying())
+		{
+			AudioComponent->Stop();
+		}
+	}
+}
+
+void AA_Barrier::SetBarrierPassword(const FText& NewPassword)
+{
+	BarrierPassword = NewPassword;
+}
+
+FText AA_Barrier::GetBarrierPassword() const
+{
+	return BarrierPassword;
 }
 
--- Plugins/ALS-Refactored-main/Source/ALSExtras/Public/DeadlockSystem/A_Barrier.h
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Public/DeadlockSystem/A_Barrier.h
@@ -1,10 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "A_Barrier.generated.h"
+
+class UBoxComponent;
 
 UCLASS()
 class ALSEXTRAS_API AA_Barrier : public AActor
@@ -12,15 +12,49 @@ class ALSEXTRAS_API AA_Barrier : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AA_Barrier();
 
 protected:
-	// Called when the game starts or when spawned
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	class UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	UAudioComponent* AudioComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	FText BarrierPassword;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* MaterialInstanceDynamic;
+
+	uint8 bIsOverlapping : 1{false};
+
+	AActor* OverlappingActor{ nullptr };
+
+	UFUNCTION()
+	void OnBarrierBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBarrierEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Setter")
+	void GetTextCommand(FText Command);
+
+	UFUNCTION(BlueprintCallable, Category = "Setter")
+	void SetBarrierPassword(const FText& NewPassword);
+
+	UFUNCTION(BlueprintCallable, Category = "Getter")
+	FText GetBarrierPassword() const;
 };
