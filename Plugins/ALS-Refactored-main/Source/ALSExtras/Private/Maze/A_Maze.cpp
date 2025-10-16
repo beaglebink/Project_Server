c31042298475d7f76d39f5272ac36d34c91e10cd
Oleg <oleh.ya@gmail.com>
2025-10-16 19:44:29 +0300

    TextureAnalysisMethods

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Private/Maze/A_Maze.cpp
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Private/Maze/A_Maze.cpp
@@ -1,4 +1,6 @@
 #include "Maze/A_Maze.h"
+#include "Engine/Texture2D.h"
+#include "Kismet/KismetRenderingLibrary.h"
 
 AA_Maze::AA_Maze()
 {
@@ -18,10 +20,34 @@ void AA_Maze::OnConstruction(const FTransform& Transform)
 	{
 		MeshDynamicMaterial = StaticMeshComponent->CreateAndSetMaterialInstanceDynamic(0);
 	}
-	if (MeshDynamicMaterial)
+	if (MeshDynamicMaterial && MazeTexture)
 	{
 		MeshDynamicMaterial->SetTextureParameterValue("Texture", MazeTexture);
 	}
+
+	//TArray<FColor> Pixels;
+	//int32 Width = MazeTexture->GetSizeX();
+	//int32 Height = MazeTexture->GetSizeY();
+
+	//FTexture2DMipMap& Mip = MazeTexture->GetPlatformData()->Mips.Last();
+	//void* Data = Mip.BulkData.Lock(LOCK_READ_ONLY);
+
+	//Pixels.SetNumUninitialized(Width * Height);
+	//FMemory::Memcpy(Pixels.GetData(), Data, Width * Height * sizeof(FColor));
+
+	//Mip.BulkData.Unlock();
+
+	//MazeArray.SetNum(Height);
+	//for (int32 Y = 0; Y < Height; ++Y)
+	//{
+	//	MazeArray[Y].Row.SetNum(Width);
+	//	for (int32 X = 0; X < Width; ++X)
+	//	{
+	//		FColor Pixel = Pixels[Y * Width + X];
+	//		MazeArray[Y].Row[X] = (Pixel.R < 128) ? -1 : 0;
+	//	}
+	//}
+	ReadMazeTextureToArray();
 }
 
 void AA_Maze::BeginPlay()
@@ -32,6 +58,33 @@ void AA_Maze::BeginPlay()
 void AA_Maze::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AA_Maze::ReadMazeTextureToArray()
+{
+	if (!MazeTexture || !MazeRenderTarget || !MeshDynamicMaterial)
+	{
+		return;
+	}
+
+	UKismetRenderingLibrary::DrawMaterialToRenderTarget(GetWorld(), MazeRenderTarget, MeshDynamicMaterial);
+
+	TArray<FLinearColor> Pixels;
+	UKismetRenderingLibrary::ReadRenderTargetRaw(GetWorld(), MazeRenderTarget, Pixels);
+
+	int32 Width = MazeRenderTarget->SizeX;
+	int32 Height = MazeRenderTarget->SizeY;
+
+	MazeArray.SetNum(Height);
+	for (int32 Y = 0; Y < Height; ++Y)
+	{
+		MazeArray[Y].Row.SetNum(Width);
+		for (int32 X = 0; X < Width; ++X)
+		{
+			FLinearColor Pixel = Pixels[Y * Width + X];
+			MazeArray[Y].Row[X] = (Pixel.R < 0.5f) ? -1 : 0;
+		}
+	}
 }
 
 void AA_Maze::HandleWeaponShot_Implementation(UPARAM(ref)FHitResult& Hit)
--- Plugins/ALS-Refactored-main/Source/ALSExtras/Public/Maze/A_Maze.h
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Public/Maze/A_Maze.h
@@ -5,6 +5,15 @@
 #include "Interfaces/I_WeaponInteraction.h"
 #include "A_Maze.generated.h"
 
+USTRUCT()
+struct FMazeRow
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<int32> Row;
+};
+
 UCLASS()
 class ALSEXTRAS_API AA_Maze : public AActor, public II_WeaponInteraction
 {
@@ -27,9 +36,17 @@ private:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 	UTexture2D* MazeTexture;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	UTextureRenderTarget2D* MazeRenderTarget;
 
 	UPROPERTY()
 	UMaterialInstanceDynamic* MeshDynamicMaterial;
+
+	UPROPERTY()
+	TArray<FMazeRow> MazeArray;
+
+	void ReadMazeTextureToArray();
 
 	void HandleWeaponShot_Implementation(UPARAM(ref)FHitResult& Hit);
 };
