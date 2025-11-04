066e055d37b368234869c50ba201eff38ad05190
Oleg <oleh.ya@gmail.com>
2025-11-04 18:47:05 +0200

    BlocksSwapAddedByTimer

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Public/EnvironmentalObstacles/A_BlocksWallObstacle.h
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Public/EnvironmentalObstacles/A_BlocksWallObstacle.h
@@ -6,6 +6,7 @@
 
 class AA_BlockObstacle;
 class UTextRenderComponent;
+enum class EDirection :uint8;
 
 UCLASS()
 class ALSEXTRAS_API AA_BlocksWallObstacle : public AActor
@@ -25,24 +26,34 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
 	TSubclassOf<AA_BlockObstacle> BlockObstacleClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
+	FIntPoint WallDimensions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true, ClampMin = "2", ClampMax = "100"))
+	int32 CriticalBlocksTreshold;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
 	uint8 bUseTimeInterval : 1{false};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true, ClampMin = "10", EditCondition = "bUseTimeInterval"))
+	float TimeIntervalBetweenSwaps = 10.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
 	uint8 bUseNumberOfBlocksDestroyed : 1{false};
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true, EditCondition = "bUseNumberOfBlocksDestroyed"))
+	int32 NumberOfBlocksDestroyedThreshold = 1;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
 	uint8 bUsePlayerAccuracy : 1{false};
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true))
-	FIntPoint WallDimensions;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true, ClampMin = "0", ClampMax = "100"))
-	int32 CriticalBlocksTreshold;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings", meta = (AllowPrivateAccess = true, EditCondition = "bUsePlayerAccuracy"))
+	int32 PlayerAccuracyThreshold = 2;
 
 	UPROPERTY()
 	TArray<AA_BlockObstacle*> WallBlocks;
@@ -50,9 +61,25 @@ private:
 	UPROPERTY()
 	FVector BlockExtent;
 
+	void PrepareBlockSwaps();
+
+	void ProcessBlockSwaps();
+
+	int32 GetNeighborIndex(int32 Index, EDirection Direction) const;
+
+	bool IsOppositeDirection(EDirection A, EDirection B) const;
+
+	EDirection GetOppositeDirection(EDirection Direction) const;
+
 	TArray<UTextRenderComponent*> DebugGridTexts;
 
 public:
+	uint8 bIsProcessingSwaps : 1{ false};
+
+	int32 BlocksOnSwapCount = 0;
+
+	void CheckAndHandleCompletedSwaps();
+
 	void UpperBlocksFall(int32 Index);
 
 	//debug function
