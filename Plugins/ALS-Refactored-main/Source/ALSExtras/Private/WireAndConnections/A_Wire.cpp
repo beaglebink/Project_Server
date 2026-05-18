eb03c27a7f1c1074af8b7588759bea3fc607d8ad
Oleg <oleh.ya@gmail.com>
2026-05-18 20:02:26 +0300

    Wire_CableComponent

--- Plugins/ALS-Refactored-main/Source/ALSExtras/Private/WireAndConnections/A_Wire.cpp
+++ Plugins/ALS-Refactored-main/Source/ALSExtras/Private/WireAndConnections/A_Wire.cpp
@@ -1,95 +1,80 @@
 #include "WireAndConnections/A_Wire.h"
-#include "Components/SplineComponent.h"
-#include "Components/SplineMeshComponent.h"
+#include "CableComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
 
 AA_Wire::AA_Wire()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+	FemaleMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FemaleMeshComponent"));
+	CableComponent = CreateDefaultSubobject<UCableComponent>(TEXT("CableComponent"));
+	ConstraintStartMiddle = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("ConstraintStartMiddle"));
+	ConstraintMiddleEnd = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("ConstraintMiddleEnd"));
+	MiddlePoint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MiddlePoint"));
 
-	SplineComponent->SetupAttachment(StaticMesh);
+	FemaleMeshComponent->SetupAttachment(StaticMesh);
+	MiddlePoint->SetupAttachment(StaticMesh);
+	CableComponent->AttachToComponent(StaticMesh, FAttachmentTransformRules::KeepRelativeTransform, "CableStart");
+	ConstraintStartMiddle->SetupAttachment(StaticMesh);
+	ConstraintMiddleEnd->SetupAttachment(MiddlePoint);
 
-	StaticMesh->SetSimulatePhysics(false);
+	FemaleMeshComponent->SetCollisionProfileName(TEXT("HighlyReactiveObject"));
+	FemaleMeshComponent->SetSimulatePhysics(true);
+	FemaleMeshComponent->SetNotifyRigidBodyCollision(true);
+	FemaleMeshComponent->BodyInstance.SetMassOverride(7.0f, true);
+	FemaleMeshComponent->SetLinearDamping(0.5f);
+	FemaleMeshComponent->SetAngularDamping(0.1f);
+
+	MiddlePoint->bHiddenInGame = true;
+	MiddlePoint->SetCollisionProfileName(TEXT("HighlyReactiveObject"));
+	MiddlePoint->SetSimulatePhysics(true);
+	MiddlePoint->SetNotifyRigidBodyCollision(true);
+	MiddlePoint->BodyInstance.SetMassOverride(7.0f, true);
+	MiddlePoint->SetLinearDamping(0.5f);
+	MiddlePoint->SetAngularDamping(0.1f);
+
+	CableComponent->EndLocation = FVector(0.0f, 0.0f, 0.0f);
+	CableComponent->CableWidth = 2.0f;
+
+	ConstraintStartMiddle->SetConstrainedComponents(StaticMesh, NAME_None, MiddlePoint, NAME_None);
+	ConstraintStartMiddle->SetAngularSwing1Limit(EAngularConstraintMotion::ACM_Free, 90.0f);
+	ConstraintStartMiddle->SetAngularSwing2Limit(EAngularConstraintMotion::ACM_Free, 90.0f);
+	ConstraintStartMiddle->SetAngularTwistLimit(EAngularConstraintMotion::ACM_Free, 5.0f);
+
+	ConstraintMiddleEnd->SetConstrainedComponents(MiddlePoint, NAME_None, FemaleMeshComponent, NAME_None);
+	ConstraintMiddleEnd->SetAngularSwing1Limit(EAngularConstraintMotion::ACM_Free, 90.0f);
+	ConstraintMiddleEnd->SetAngularSwing2Limit(EAngularConstraintMotion::ACM_Free, 90.0f);
+	ConstraintMiddleEnd->SetAngularTwistLimit(EAngularConstraintMotion::ACM_Free, 5.0f);
 }
 
 void AA_Wire::OnConstruction(const FTransform& Transform)
 {
 	Super::OnConstruction(Transform);
 
-	SplineComponent->ClearSplinePoints();
-	for (USplineMeshComponent* SplineMesh : SplineMeshComponents)
-	{
-		if (SplineMesh)
-		{
-			SplineMesh->DestroyComponent();
-		}
-	}
-	SplineMeshComponents.Empty();
+	MiddlePoint->SetRelativeLocation(FemaleMeshComponent->GetRelativeLocation() / 2.0f);
 
-	if (WireMesh)
-	{
-		WireMeshLength = WireMesh->GetBounds().BoxExtent.X * 2;
-		SplineComponent->AddSplinePoint(FVector::ZeroVector, ESplineCoordinateSpace::Local);
-		FVector Dir = SplineComponent->GetRotationAtSplinePoint(0, ESplineCoordinateSpace::Local).Vector().GetSafeNormal();
-		FVector PointLocation = SplineComponent->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::Local) + Dir * WireMeshLength;
-		SplineComponent->AddSplinePoint(PointLocation, ESplineCoordinateSpace::Local);
+	CableComponent->SetAttachEndToComponent(FemaleMeshComponent, "CableEnd");
+	CableComponent->CableLength = FemaleMeshComponent->GetRelativeLocation().Length() + 50.0f;
 
-		for (int32 i = 2; i <= NumberOfSegments; ++i)
-		{
-			Dir = SplineComponent->GetRotationAtSplinePoint(i - 1, ESplineCoordinateSpace::Local).Vector().GetSafeNormal();
-			FRotator RandomRot = FRotator(0.0f, FMath::RandRange(-PointNoise, PointNoise), 0.0f);
-			FVector NoisyDir = RandomRot.RotateVector(Dir);
-			PointLocation = SplineComponent->GetLocationAtSplinePoint(i - 1, ESplineCoordinateSpace::Local) + NoisyDir * WireMeshLength;
-			SplineComponent->AddSplinePoint(PointLocation, ESplineCoordinateSpace::Local);
-		}
+	ConstraintStartMiddle->SetLinearXLimit(ELinearConstraintMotion::LCM_Limited, CableComponent->CableLength / 2.0f);
+	ConstraintStartMiddle->SetLinearYLimit(ELinearConstraintMotion::LCM_Free, CableComponent->CableLength / 2.0f);
+	ConstraintStartMiddle->SetLinearZLimit(ELinearConstraintMotion::LCM_Free, CableComponent->CableLength / 2.0f);
 
-		for (int32 i = 1; i <= NumberOfSegments; ++i)
-		{
-			USplineMeshComponent* SplineMesh = NewObject<USplineMeshComponent>(this);
-			SplineMeshComponents.Add(SplineMesh);
-			SplineMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-			SplineMesh->SetForwardAxis(ESplineMeshAxis::X);
-			SplineMesh->SetupAttachment(RootComponent);
-			SplineMesh->RegisterComponent();
-			SplineMesh->SetStaticMesh(WireMesh);
-			SplineMesh->SetStartAndEnd(SplineComponent->GetLocationAtSplinePoint(i - 1, ESplineCoordinateSpace::World), SplineComponent->GetTangentAtSplinePoint(i - 1, ESplineCoordinateSpace::World),
-				SplineComponent->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World), SplineComponent->GetTangentAtSplinePoint(i, ESplineCoordinateSpace::World));
-		}
-	}
+	ConstraintMiddleEnd->SetLinearXLimit(ELinearConstraintMotion::LCM_Limited, CableComponent->CableLength / 2.0f);
+	ConstraintMiddleEnd->SetLinearYLimit(ELinearConstraintMotion::LCM_Free, CableComponent->CableLength / 2.0f);
+	ConstraintMiddleEnd->SetLinearZLimit(ELinearConstraintMotion::LCM_Free, CableComponent->CableLength / 2.0f);
 }
 
 void AA_Wire::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	MoveSplinePoints(DeltaTime);
-
-	UpdateSplineMeshes();
 }
 
 void AA_Wire::BeginPlay()
 {
 	Super::BeginPlay();
 
-}
-
-void AA_Wire::MoveSplinePoints(float DeltaTime)
-{
-	SplineComponent->SetLocationAtSplinePoint(0, StaticMesh->GetComponentLocation(), ESplineCoordinateSpace::World);
-}
-
-void AA_Wire::UpdateSplineMeshes()
-{
-	int32 NumPoints = SplineComponent->GetNumberOfSplinePoints();
-
-	for (int i = 0; i < NumPoints - 1; ++i)
-	{
-		FVector Start = SplineComponent->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
-		FVector End = SplineComponent->GetLocationAtSplinePoint(i + 1, ESplineCoordinateSpace::World);
-
-		FVector Tangent = (End - Start);
-
-		SplineMeshComponents[i]->SetStartAndEnd(Start, Tangent, End, Tangent);
-	}
+	CableComponent->SetAttachEndToComponent(FemaleMeshComponent, "CableEnd");
 }
